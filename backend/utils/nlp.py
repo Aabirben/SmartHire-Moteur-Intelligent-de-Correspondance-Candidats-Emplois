2ca295f38c1c15e7ed75f22c1fe234b07c76492f
BENHAMAMOUCHE AABIR <ben10aabir@gmail.com>
2025-12-04 23:30:51 +0100

    Delete backend directory

--- backend/utils/nlp.py
+++
@@ -1,4 +0,0 @@
-# Placeholder for NLP utilities
-def extract_skills(text):
-    # TODO: Implement full NLP pipeline with spaCy (lemmatization, entity extraction)
-    return ["skill1", "skill2"]  # Placeholder
